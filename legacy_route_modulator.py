fa2100343c517261904f2fbe26f323f21e365f56
Deven Bansod <devenbansod.bits@gmail.com>
2016-03-22 11:07:43 +0530

    Add legacy route modulator
    Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

---
+++ legacy_route_modulator.py
@@ -0,0 +1,41 @@
+import DBConnection
+import TelnetDriver
+
+class legacyRouteMod:
+	ipRange = []
+	route = []
+
+	def __init__ (self, map, path):
+		self.ipRange = map
+		self.route = reversed(path)
+
+	def getOutputInterface(src, dst):
+		# return the interface in src which connects to dst
+		# query database for this
+		conn = DBConnection('test_db')
+		interface = conn.getInterfaceConnectedTo(src, dst)
+
+		# the interface 'name' is interface[5]
+		return interface[5]
+
+
+	def addRoutes():
+		for i in range(1, len(route)):
+			# get output interface information
+			intf = getOutputInterface(self.route[i], self.route[i-1])
+
+			# Call expect script to add route to the router
+			# set network (ipRange[3]) output to intf
+			tD = TelnetDriver()
+			success = td.addStaticRoute(
+				self.route[i], self.ipRange, next_router=self.route[i-1], interface=intf
+			)
+
+			if (success == False):
+				# remove previously added routes
+				# call 'tD.removeStaticRoute()' in a loop over all previous routers
+
+				# and break
+				break;
+
+
